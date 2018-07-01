86584f9c6b748745d90e3c394f3d1c6aac4dca69
Seth Toles <sethtoles@gmail.com>
2018-07-01 13:51:49 -0700

    restructure blocks, clean up paper

--- index.js
+++ index.js
@@ -1,78 +1,97 @@
-// Util
+// CONFIGURATION
+const SPEED_LIMIT = 10; // The maximum pixel distance the word can move in one frame
+const FONT_SIZE = 100;
+const KEY_EXP = /^[\w\s]$/; // Valid characters to be used in the word
+const EDITING_TIME = 2000; // How long after each character typed before leaving editing mode
+const MOUSE_FOLLOW_TIME = 500; // How long the text will follow the mouse after it stops moving
+
+
+// UTIL
 const hsla = (h, s, l, a = 1) => `hsla(${h}, ${s}%, ${l}%, ${a})`; // hsla string
-const randHue = () => Math.floor(Math.random() * 360); // random int 0 to 360
-const randPercent = () => Math.floor(Math.random() * 100); // random int 0 to 100
-const randAdjust = () => Math.round(Math.random() * 2) - 1; // random int -1, 0, or 1
-const fade = (ctx, amount = 0.1) => {
-    ctx.fillStyle = hsla(0, 0, 0, amount);
-    ctx.fillRect(0, 0, width, height);
-}; // apply translucent black rect over canvas
-const scramble = string => {
-    const letters = [ ...string ];
-    return letters.sort(() => Math.random() < 0.5 ? 1 : -1).join('');
-}; // rearrange letters in word
+const randHue = () => Math.floor(Math.random() * 360); // Random int 0 to 360
+const randPercent = () => Math.floor(Math.random() * 100); // Random int 0 to 100
+const randAdjust = () => Math.round(Math.random() * 2) - 1; // Random int -1, 0, or 1
+const scramble = string => [ ...string ].sort(() => Math.random() < 0.5 ? 1 : -1).join(''); // Rearrange letters in word
+
+
+// STATE SETUP
+let width;
+let height;
+let word;
+let scrambledWord;
+let shouldUseScrambled = false;
+let position = [0, 0];
+let vector = [0, 0];
+let color = hsla(randHue(), randPercent(), randPercent());
+let textWidth;
+let textHeight;
+
+
+// CANVAS SETUP
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+
+// STATE MUTATION HELPERS
+
+// Resize the canvas and set global width/height
+const setBounds = () => {
+    width = canvas.width = window.innerWidth;
+    height = canvas.height = window.innerHeight;
+    context.font = `${FONT_SIZE}px Courier, monospace`;
+    context.textAlign = 'left';
+    context.textBaseline = 'top';
+};
+
+// Update global vars based on new word
 const setWord = string => {
     word = string;
     scrambledWord = scramble(word);
     textWidth = context.measureText(word).width;
-    maxXPosition = width - textWidth;
-    maxYPosition = height - textHeight;
-}; // Update global vars based on new word
+    textHeight = FONT_SIZE;
+};
+
+// Set the global position so that the word is centered
 const centerWord = () => {
     position = [
         (width / 2) - (textWidth / 2),
         (height / 2) - (textHeight / 2),
     ];
-}; // Set the global position so that the word is centered
+};
 
-// Global Setup
-const {
-    innerWidth: width,
-    innerHeight: height,
-} = window;
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
 
-const keyExp = /^[\w\s]$/; // valid characters to be used in the word
-
-const SPEED_LIMIT = 10; // the maximum pixel distance the word can move in one frame
-const FONT_SIZE = 100;
-
-// Canvas Setup
-Object.assign(canvas, { width, height });
-context.font = `${FONT_SIZE}px Courier, monospace`;
-context.textAlign = 'left';
-context.textBaseline = 'top';
-
+// TEXT EDITING
 let editing = false;
 let editingTimeout;
-document.addEventListener('keyup', e => {
+const editText = e => {
+    e.preventDefault();
     const { key } = e;
 
-    if (!key || !key.match(keyExp)) {
+    if (!key || !key.match(KEY_EXP)) {
         return;
     }
 
-    e.preventDefault();
-
-    // editing not in progress, clear word and enter editing mode
+    // Editing not in progress, clear word and enter editing mode
     if (!editing) {
         editing = true;
         word = '';
     }
 
+    // Add new letter to word
     setWord(word + key);
 
-    // clear and restart editing timeout
+    // Clear and restart editing timeout
     clearTimeout(editingTimeout);
     editingTimeout = setTimeout(() => {
         editing = false;
-    }, 2000);
-});
+    }, EDITING_TIME);
+};
 
+
+// MOUSE TARGETING
 let target = null;
 let targetingTimeout;
-document.addEventListener('mousemove', ({ clientX, clientY }) => {
+const setTarget = ({ clientX, clientY }) => {
     target = {
         x: clientX,
         y: clientY,
@@ -81,25 +100,15 @@ document.addEventListener('mousemove', ({ clientX, clientY }) => {
     clearTimeout(targetingTimeout);
     targetingTimeout = setTimeout(() => {
         target = null;
-    }, 500);
-});
+    }, MOUSE_FOLLOW_TIME);
+};
 
-// State Setup
-let word;
-let scrambledWord;
-let shouldUseScrambled = false;
-let position = [0, 0];
-let vector = [0, 0];
-let iterations = 0;
-let maxXPosition;
-let maxYPosition;
-let color = hsla(randHue(), randPercent(), randPercent());
 
-const textHeight = FONT_SIZE;
-
+// FRAME RENDERER
 const draw = () => {
     // Fade existing content
-    fade(context, 0.03 );
+    context.fillStyle = hsla(0, 0, 0, 0.03);
+    context.fillRect(0, 0, width, height);
 
     // Scramble/restore word
     if (Math.random() < 0.005) {
@@ -125,11 +134,11 @@ const draw = () => {
         dy += diffY * 0.01;
     }
 
-    // Calculate new position
+    // Set new position within bounds
     const newX = x + dx;
     const newY = y + dy;
-
-    // Set new position within bounds
+    const maxXPosition = width - textWidth;
+    const maxYPosition = height - textHeight;
     position = [
         Math.max(0, Math.min(newX, maxXPosition)),
         Math.max(0, Math.min(newY, maxYPosition)),
@@ -154,13 +163,18 @@ const draw = () => {
     context.fillText(shouldUseScrambled ? scrambledWord : word, ...position);
 
     // Repeat
-    iterations++;
     requestAnimationFrame(draw);
-}
+};
 
-document.addEventListener('click', () => fade(context, 1));
 
-// Start
+// EVENT LISTENERS
+window.addEventListener('resize', setBounds);
+document.addEventListener('keyup', editText);
+document.addEventListener('mousemove', setTarget);
+
+
+// START
+setBounds();
 setWord('SCRAMPAGE');
 centerWord();
 draw();
